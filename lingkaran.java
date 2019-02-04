62181807492c28a9c9405eaaea87c13532340645
swd-tea <47308564+swd-tea@users.noreply.github.com>
2019-02-04 08:32:37 +0700

    Add files via upload

---
+++ lingkaran.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class lingkaran
+{
+public static void main(String[] args)
+
+{
+Scanner input = new Scanner(System.in);
+
+double phi = 3.14;
+double r, luas,keliling;
+
+System.out.println("Program Luas Lingkaran\n");
+System.out.print("Masukkan Panjang Jari-jari : ");
+r = input.nextDouble();
+
+luas = 0.5 * phi * r * r;
+keliling = 2 * phi * r;
+
+System.out.print("Luas Lingkaran = " + (int)luas + " \nKeliling lingkaran = "+ (int)keliling);
+}
+}
